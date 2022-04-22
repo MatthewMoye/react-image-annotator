92fe5fb5b43a212d04de813e5205ace37577066c
Matthew M <mattemoye@gmail.com>
2022-04-21 21:36:27 -0400

    begin adding right click pan

--- src/App.js
+++ src/App.js
@@ -4,17 +4,35 @@ import { useRef, useState } from "react";
 
 const App = () => {
   const [mousePos, setmousePos] = useState({ x: 0, y: 0 });
+  const [containerPos, setContainerPos] = useState({ x: 0, y: 0 });
+  const [pan, setPan] = useState(false)
   const [imgData, setimgData] = useState([]);
 
   const imgRef = useRef();
+  const containerRef = useRef();
 
-  const getPos = (e) => {
+  const getPos = (e, ref) => {
+    if (e.button !== 2) return
+    setPan(true)
     // get mousePosition regardless of zoom level or page position for current image
     setmousePos({
-      x: e.pageX - imgRef.current.offsetLeft,
-      y: e.pageY - imgRef.current.offsetTop,
+      x: e.pageX - ref.current.offsetLeft,
+      y: e.pageY - ref.current.offsetTop,
     });
   };
+
+  const updatePosition = (e, ref) => {
+    const moveX = mousePos.x - (e.pageX - ref.current.offsetLeft);
+    const moveY = mousePos.y - (e.pageY - ref.current.offsetTop);
+    if (pan) {
+      getPos(e, ref);
+      setContainerPos({x: containerPos.x - moveX, y: containerPos.y - moveY})
+    }
+  };
+
+  const stopMovingPosition = () => {
+    setPan(false)
+  }
 
   const onImgLoad = (e) => {
     // make sure this works with varying zoom lvls
@@ -25,12 +43,26 @@ const App = () => {
   };
 
   return (
-    <>
-      <div className="App" onMouseMove={getPos}>
+    <div
+      className="App"
+      onMouseDown={(e) => getPos(e, containerRef)}
+      onMouseMove={(e) => updatePosition(e, containerRef)}
+      onMouseUp={stopMovingPosition}
+      onContextMenu={(e)=> e.preventDefault()}
+      draggable={false}
+    >
+      <div
+        style={{
+          position: "absolute",
+          top: `${containerPos.y}px`,
+          left: `${containerPos.x}px`,
+        }}
+        ref={containerRef}
+      >
         <Image imgRef={imgRef} onImgLoad={onImgLoad} />
       </div>
       {mousePos.x}
-    </>
+    </div>
   );
 };
 
--- src/components/Layer/Image.js
+++ src/components/Layer/Image.js
@@ -1,11 +1,19 @@
 const Image = (props) => {
   return (
     <img
-    id={123}
+      id={123}
       ref={props.imgRef}
       src="https://jw-webmagazine.com/wp-content/uploads/2020/03/Kimetsu-no-YaibaDemon-Slayer.jpg"
       alt="new"
       onLoad={props.onImgLoad}
+      onContextMenu={(e)=> e.preventDefault()}
+      style={{
+        userSelect: "none",
+        MozUserSelect: "none",
+        WebkitUserSelect: "none",
+        msUserSelect: "none",
+        pointerEvents: "none",
+      }}
     />
   );
 };
