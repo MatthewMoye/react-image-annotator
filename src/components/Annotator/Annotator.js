0bf8fb2ec941c965918292e66ef7a8ba46d7fc3e
Matthew M <mattemoye@gmail.com>
2022-05-18 23:48:12 -0400

    cleanup

--- src/components/Annotator/Annotator.js
+++ src/components/Annotator/Annotator.js
@@ -2,6 +2,9 @@ import { useRef, useState } from "react";
 import Image from "../Layer/Image";
 import styles from "./Annotator.module.css";
 
+const tempImg =
+  "https://jw-webmagazine.com/wp-content/uploads/2020/03/Kimetsu-no-YaibaDemon-Slayer.jpg";
+
 const Annotator = () => {
   const [mousePos, setmousePos] = useState({ x: 0, y: 0 });
   const [containerPos, setContainerPos] = useState({ x: 0, y: 0 });
@@ -11,26 +14,25 @@ const Annotator = () => {
   const imgRef = useRef();
   const containerRef = useRef();
 
-  const getPos = (e, ref) => {
+  const enablePanning = (e) => {
     if (e.button !== 2) return;
-    setPan(true);
-    // get mousePosition regardless of zoom level or page position for current image
     setmousePos({
-      x: e.pageX - ref.current.offsetLeft,
-      y: e.pageY - ref.current.offsetTop,
+      x: e.pageX - containerRef.current.offsetLeft,
+      y: e.pageY - containerRef.current.offsetTop,
     });
+    setPan(true);
   };
 
-  const updatePosition = (e, ref) => {
-    const moveX = mousePos.x - (e.pageX - ref.current.offsetLeft);
-    const moveY = mousePos.y - (e.pageY - ref.current.offsetTop);
+  const updatePosition = (e) => {
     if (pan) {
-      getPos(e, ref);
+      const moveX = mousePos.x - (e.pageX - containerRef.current.offsetLeft);
+      const moveY = mousePos.y - (e.pageY - containerRef.current.offsetTop);
       setContainerPos({ x: containerPos.x - moveX, y: containerPos.y - moveY });
     }
   };
 
-  const stopMovingPosition = () => {
+  const disablePanning = (e) => {
+    if (e.button !== 2) return;
     setPan(false);
   };
 
@@ -45,9 +47,9 @@ const Annotator = () => {
   return (
     <div
       className={styles.annotatorContainer}
-      onMouseDown={(e) => getPos(e, containerRef)}
-      onMouseMove={(e) => updatePosition(e, containerRef)}
-      onMouseUp={stopMovingPosition}
+      onMouseDown={enablePanning}
+      onMouseMove={updatePosition}
+      onMouseUp={disablePanning}
       onContextMenu={(e) => e.preventDefault()}
     >
       <div
@@ -55,13 +57,17 @@ const Annotator = () => {
         style={{
           top: `${containerPos.y}px`,
           left: `${containerPos.x}px`,
+          width:
+            "1000px" /* make sure image children fit in width (keep zoom in mind) */,
+          height:
+            "1000px" /* make sure image children fit in height (keep zoom in mind) */,
         }}
         ref={containerRef}
       >
-        <Image imgRef={imgRef} onImgLoad={onImgLoad} />
-        <Image imgRef={imgRef} onImgLoad={onImgLoad} />
+        <Image id={11} imgRef={imgRef} src={tempImg} onImgLoad={onImgLoad} />
+        <Image id={22} imgRef={imgRef} src={tempImg} onImgLoad={onImgLoad} />
       </div>
-      {/* {mousePos.x} */}
+      {mousePos.x}
     </div>
   );
 };
--- src/components/Layer/Image.js
+++ src/components/Layer/Image.js
@@ -3,15 +3,13 @@ import styles from "./Image.module.css";
 const Image = (props) => {
   return (
     <img
-      id={123}
+      id={props.id}
       ref={props.imgRef}
-      src={"https://jw-webmagazine.com/wp-content/uploads/2020/03/Kimetsu-no-YaibaDemon-Slayer.jpg"}
+      src={props.src}
       alt={"cool stuff"}
       onLoad={props.onImgLoad}
       onContextMenu={(e) => e.preventDefault()}
       className={styles.image}
-      width={1000}
-      height={1000}
     />
   );
 };
