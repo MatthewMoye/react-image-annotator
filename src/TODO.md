b1431acdab2ad4af1e73d0770d2173c3df8b1c4f
Matthew M <mattemoye@gmail.com>
2022-06-05 01:39:27 -0400

    fix image spacing bug

--- src/TODO.md
+++ src/TODO.md
@@ -1,6 +1,4 @@
 Unit tests
-Make images dragable
 Add zooming
 Implement tools, useMemo on shapes
-Fix image spacing
 Fix bug when trying to move img while panning
--- src/components/Workspace/Workspace.js
+++ src/components/Workspace/Workspace.js
@@ -33,32 +33,43 @@ const Workspace = ({
     <div className={styles.imgList} style={windowSize} ref={imageContainerRef}>
       {images.map((img, imgIdx) => {
         const isActiveImg = activeImageIdx === imgIdx;
+        const imgMargin = { width: 0.1 * img.width, height: 0.1 * img.height };
         return (
           <div
-            key={`img-container${img.id}`}
-            onMouseDown={(e) => handleImgMouseDown(e, imgIdx)}
-            className={styles.imgContainer}
-            style={{ zIndex: isActiveImg ? 1 : 0 }}
-            ref={isActiveImg ? activeImageRef : null}
+            key={`img-container-${img.id}`}
+            className={styles.container}
+            style={{
+              minWidth: `${img.width}px`,
+              minHeight: `${img.height}px`,
+              margin: `${imgMargin.height}px ${imgMargin.width}px`,
+            }}
           >
-            <img
-              key={`img-${img.id}`}
-              id={img.id}
-              src={img.src}
-              alt={img.alt}
-              onLoad={onImgLoad}
-              style={{ zIndex: isActiveImg ? 1 : 0 }}
-              className={styles.image}
-            />
-            <svg
-              key={`annotation-list-${img.id}`}
-              className={styles.annotationList}
-              style={{
-                zIndex: isActiveImg ? 1 : 0,
-                width: `${img.width}px`,
-                height: `${img.height}px`,
-              }}
-            ></svg>
+            <div
+              key={`img-box-${img.id}`}
+              onMouseDown={(e) => handleImgMouseDown(e, imgIdx)}
+              className={styles.imgBox}
+              style={{ zIndex: isActiveImg ? 1 : 0, top: 0, left: 0 }}
+              ref={isActiveImg ? activeImageRef : null}
+            >
+              <img
+                key={`img-${img.id}`}
+                id={img.id}
+                src={img.src}
+                alt={img.alt}
+                onLoad={onImgLoad}
+                style={{ zIndex: isActiveImg ? 1 : 0 }}
+                className={styles.image}
+              />
+              <svg
+                key={`annotation-list-${img.id}`}
+                className={styles.annotationList}
+                style={{
+                  zIndex: isActiveImg ? 1 : 0,
+                  width: `${img.width + imgMargin.width * 2}px`,
+                  height: `${img.height + imgMargin.height * 2}px`,
+                }}
+              ></svg>
+            </div>
           </div>
         );
       })}
